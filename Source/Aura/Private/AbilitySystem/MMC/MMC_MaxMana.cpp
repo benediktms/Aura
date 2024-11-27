bfe639444fe6ef1ab9f86a6cf696def922f1b91e
Benedikt Schnatterbeck <benediktms@hey.com>
2024-11-27 08:50:08 +0000

    feat(secondary-attributes): add max mana mmc

---
+++ Source/Aura/Private/AbilitySystem/MMC/MMC_MaxMana.cpp
@@ -0,0 +1,35 @@
+// Copyright Benedikt Schnatterbeck
+
+
+#include "AbilitySystem/MMC/MMC_MaxMana.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_MaxMana::UMMC_MaxMana()
+{
+	IntelligenceDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();
+	IntelligenceDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+
+	IntelligenceDef.bSnapshot = false;
+}
+
+float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float IntelligenceMagnitude = 0.f;
+	GetCapturedAttributeMagnitude(IntelligenceDef, Spec, EvaluationParameters, IntelligenceMagnitude);
+	IntelligenceMagnitude = FMath::Max(IntelligenceMagnitude, 0.f);
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetActorLevel();
+
+	return 100 + 2.5 * IntelligenceMagnitude + 10 * PlayerLevel;
+}
