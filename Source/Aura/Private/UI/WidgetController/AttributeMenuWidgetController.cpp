2be1eee512075487676f5af80f817f76a98f9d12
Benedikt Schnatterbeck <benediktms@hey.com>
2024-12-11 21:12:09 +0000

    feat(attribute-menu): broadcast initial attribute values to menu widget

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,10 +3,23 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+
 void UAttributeMenuWidgetController::BindCallbackToDependencies()
 {
 }
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+	check(AttributeInfo);
+
+	for (auto& [Tag, AttributeFnPtr] : AS->TagsToAttributes)
+	{
+		FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(Tag);
+		Info.AttributeValue = AttributeFnPtr().GetNumericValue(AS);
+		AttributeInfoDelegate.Broadcast(Info);
+	}
 }
--- Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -6,6 +6,11 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "AttributeMenuWidgetController.generated.h"
 
+class UAttributeInfo;
+struct FAuraAttributeInfo;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignature, const FAuraAttributeInfo&, AttributeInfo);
+
 /**
  * 
  */
@@ -15,6 +20,13 @@ class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
 	GENERATED_BODY()
 
 public:
-	virtual void BindCallbackToDependencies() override;
 	virtual void BroadcastInitialValues() override;
+	virtual void BindCallbackToDependencies() override;
+
+	UPROPERTY(BlueprintAssignable, Category="GAS|Attribute")
+	FAttributeInfoSignature AttributeInfoDelegate;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category="GAS|Attribute")
+	TObjectPtr<UAttributeInfo> AttributeInfo;
 };
