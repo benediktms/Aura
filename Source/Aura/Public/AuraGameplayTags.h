d686192441b22bbe21ea6ce18be4c4d5769801ae
Benedikt Schnatterbeck <benediktms@hey.com>
2024-12-17 15:06:22 +0000

    feat(input): add input tags and actions for abilities

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -35,6 +35,13 @@ public:
 	FGameplayTag Attributes_Secondary_CriticalHitDamage;
 	FGameplayTag Attributes_Secondary_CriticalHitResistance;
 
+	FGameplayTag InputTag_LMB;
+	FGameplayTag InputTag_RMB;
+	FGameplayTag InputTag_1;
+	FGameplayTag InputTag_2;
+	FGameplayTag InputTag_3;
+	FGameplayTag InputTag_4;
+
 private:
 	static FAuraGameplayTags GameplayTags;
 };
