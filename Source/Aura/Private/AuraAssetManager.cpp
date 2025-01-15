e0d2ef5f3b2797d0c5de2d196eccb0413665ba2c
Benedikt Schnatterbeck <benediktms@hey.com>
2025-01-15 21:53:32 +0000

    feat(fire-bolt): replicate ability activation down to server

--- Source/Aura/Private/AuraAssetManager.cpp
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -2,6 +2,8 @@
 
 
 #include "AuraAssetManager.h"
+
+#include "AbilitySystemGlobals.h"
 #include "AuraGameplayTags.h"
 
 UAuraAssetManager& UAuraAssetManager::Get()
@@ -15,6 +17,10 @@ UAuraAssetManager& UAuraAssetManager::Get()
 void UAuraAssetManager::StartInitialLoading()
 {
 	Super::StartInitialLoading();
+
+	// this is no longer needed as of UE5.3
+	// see: https://github.com/tranek/GASDocumentation?tab=readme-ov-file#491-initglobaldata
+	// UAbilitySystemGlobals::Get().InitGlobalData();
 
 	FAuraGameplayTags::InitializeNativeGameplayTags();
 }
--- Source/Aura/Public/AbilitySystem/TargetDataUnderMouse.h
+++ Source/Aura/Public/AbilitySystem/TargetDataUnderMouse.h
@@ -6,7 +6,7 @@
 #include "Abilities/Tasks/AbilityTask.h"
 #include "TargetDataUnderMouse.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FVector&, Data);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FGameplayAbilityTargetDataHandle&, Data);
 
 /**
  *
@@ -34,4 +34,7 @@ public:
 
 private:
 	virtual void Activate() override;
+	void SendMouseCursorData();
+
+	void OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag);
 };
